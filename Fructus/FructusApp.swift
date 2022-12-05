7915bc535e2ae898cd923cd53688d2e51188a1db
Olha Khomlyak <olga.khomlyak@gmail.com>
2022-12-05 20:06:20 +0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Olha Khomlyak on 5.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
